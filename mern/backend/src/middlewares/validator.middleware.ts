806800113e02bad97918017b22e5eddcd3f52118
Mohd Usama <mohd.usama@pixelvide.com>
2024-10-30 14:39:51 +0530

    add book model and validation for model

--- mern/backend/src/middlewares/validator.middleware.ts
+++ mern/backend/src/middlewares/validator.middleware.ts
@@ -28,6 +28,81 @@ export const newAuthorSchema= z.object({
     .optional(),
 });
 
+export const newBookSchema = z.object({
+    title: z.string({
+        required_error:"Title is missing!",
+        invalid_type_error: "Invalid title!"
+    }).trim(),
+    description: z.string({
+        required_error:"Description is missing!",
+        invalid_type_error: "Invalid Description!"
+    }).trim(),
+    language: z.string({
+        required_error:"Language is missing!",
+        invalid_type_error: "Invalid Language!"
+    }).trim(),
+    publishedAt: z.coerce.date({
+        required_error:"Published Date is missing!",
+        invalid_type_error: "Invalid Published Date!"
+    }),
+    publicationName: z.string({
+        required_error:"Publication Name is missing!",
+        invalid_type_error: "Invalid Publication Name!"
+    }).trim(),
+    genre: z.string({
+        required_error:"Genre is missing!",
+        invalid_type_error: "Invalid Genre!"
+    }).trim(),
+    price: z.string({
+        required_error:"Price is missing!",
+        invalid_type_error: "Invalid Price!"
+    }).transform((value, ctx)=>{
+        try{
+            return JSON.parse(value);
+        }catch(error){
+            ctx.addIssue({code: "custom", message:"Invalid Price Data"});
+            return z.NEVER;
+        }
+    }).pipe(
+        z.object({
+            mrp: z.number({
+                required_error:"MRP is missing!",
+                invalid_type_error: "Invalid MRP!"
+            }).nonnegative('Invalid MPR!'),
+            sale: z.number({
+                required_error:"Sale price is missing!",
+                invalid_type_error: "Invalid Sale price!"
+            }).nonnegative('Invalid Sale price!')
+        })
+    ).refine((price)=> price.sale <= price.mrp, "Sale price should be less than MRP"),
+    fileInfo: z.string({
+        required_error:"File info is missing!",
+        invalid_type_error: "Invalid File info!"
+    }).transform((value, ctx)=>{
+        try{
+            return JSON.parse(value);
+        }catch(error){
+            ctx.addIssue({code: "custom", message:"Invalid File info"});
+            return z.NEVER;
+        }
+    }).pipe(
+        z.object({
+            name: z.string({
+                required_error:"FileInfo.name is missing!",
+                invalid_type_error: "Invalid FileInfo.name!"
+            }).trim(),
+            type: z.string({
+                required_error:"FileInfo.type is missing!",
+                invalid_type_error: "Invalid FileInfo.type!"
+            }).trim(),
+            size: z.number({
+                required_error:"FileInfo.size is missing!",
+                invalid_type_error: "Invalid FileInfo.size!"
+            }).nonnegative('Invalid FileInfo.size!')
+        })
+    )
+  })
+
 export const validate = <T extends unknown>(schema: ZodType<T>): RequestHandler => {
     return (req, res, next): void => {
 
---
+++ mern/backend/src/models/book.model.ts
@@ -0,0 +1,102 @@
+import { model, Schema } from "mongoose";
+import { ObjectId } from "mongoose";
+
+export interface BookDoc {
+    author: ObjectId;
+    title: string;
+    slug: string;
+    description: string;
+    language: string;
+    publishedAt: Date;
+    publicationName: string;
+    genre: string;
+    price: {
+        mrp: number;
+        sale: number;
+    };
+    cover?: {
+        id: string;
+        url: number;
+    };
+    fileInfo: {
+        id: string;
+        size: number;
+    };
+}
+
+const bookSchema = new Schema<BookDoc>({
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: "Author",
+        required: true,
+    },
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    slug: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+    },
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    language: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    publicationName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    genre: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    publishedAt: {
+        type: Date,
+        required: true,
+    },
+    price: {
+        type: Object,
+        required: true,
+        mrp: {
+            type: Number,
+            required: true,
+        },
+        sale: {
+            type: Number,
+            required: true,
+        }
+    },
+    cover: {
+       url: String,
+       id: String
+    },
+    fileInfo: {
+        type: Object,
+        required: true,
+        url: {
+            type: String,
+            required: true,
+        },
+        sale: {
+            id: String,
+            required: true,
+        }
+    },
+
+
+});
+
+const BookModel = model('Book', bookSchema);
+
+export default BookModel
